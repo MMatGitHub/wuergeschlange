3c96b0c558b77ad4d9c6f8fedafc3dfc2c6cfc08
Mi Ma <mima@sein.io>
2024-04-27 17:57:32 +0200

    asdasd

--- wuergparallel/job_pong.py
+++ wuergparallel/job_pong.py
@@ -2,7 +2,7 @@ import os
 import job
 def getDefinition():
     einJob=job
-    einJob.mit="ping google -c 6000  >> wuerglog.log 2>&1"
+    einJob.mit="ping google -c 2  >> wuerglog.log 2>&1"
     einJob.algorithmus = lambda x: os.system(x)
     return einJob
 
